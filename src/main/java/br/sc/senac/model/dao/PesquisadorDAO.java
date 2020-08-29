f2222a929e9d91c1af6a4d919228c36ae97ed8b0
rosdebiasi <rosanadebiasi@gmail.com>
2020-08-29 19:55:56 -0300

    Primeiro commit e push do projeto:
    - padrão do projeto Maven;
    - adicionada a tabela referente ao banco de dados. Obs: a extensão do
    arquivo é em .txt porque deu problema em .sql;
    - adicionadas as classes VO e DAO;

--- src/main/java/br/sc/senac/model/dao/PesquisadorDAO.java
+++ src/main/java/br/sc/senac/model/dao/PesquisadorDAO.java
@@ -1,5 +1,33 @@
 package br.sc.senac.model.dao;
 
+import br.sc.senac.model.vo.Pesquisador;
+
 public class PesquisadorDAO {
+	
+	public Pesquisador cadastrar(Pesquisador novoPesquisador) {
+		
+		// método
+		
+		return novoPesquisador;
+	}
+	
+	public boolean alterar (Pesquisador pesquisador) {
+		
+		return true;
+	}
+	
+	public boolean excluir (int id) {
+		
+		return true;
+	}
+	
+	public Pesquisador pesquisarPorId(int id) {
+		
+		Pesquisador pesquisadorBuscado = null;
+		
+		return pesquisadorBuscado;
+		
+	}
+	
 
 }
